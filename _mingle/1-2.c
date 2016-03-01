7c0b07f7627c948bdb69231c7bfd86315ee08e0e
Mingle Chang <adadminmin@tsunamideMac-mini-2.local>
2016-03-01 12:27:10 +0800

    link .a

--- _mingle/1-2.c
+++ _mingle/1-2.c
@@ -6,14 +6,14 @@ int main(int argc,char **argv){
 	struct sockaddr_in serveraddr;
 
 	if(argc != 2){
-		// err_quit("usage: a.out <IPAddress>");
-		printf("usage: a.out <IPAddress>");
+		err_quit("usage: a.out <IPAddress>");
+		// printf("usage: a.out <IPAddress>");
 		exit(1);
 	}
 
 	if((sockfd = socket(AF_INET,SOCK_STREAM,0))<0){
-		// err_sys("socket error");
-		printf("socket error");
+		err_sys("socket error");
+		// printf("socket error");
 		exit(1);
 	}
 
@@ -21,15 +21,15 @@ int main(int argc,char **argv){
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_port = htons(13);
 	if (inet_pton(AF_INET,argv[1],&serveraddr.sin_addr)<=0){
-		// err_quit("inet_pton error for %s",argv[1]);
-		printf("inet_pton error for %s",argv[1]);
+		err_quit("inet_pton error for %s",argv[1]);
+		// printf("inet_pton error for %s",argv[1]);
 		exit(1);
 	}
 
 	if (connect(sockfd,(SA *)&serveraddr,sizeof(serveraddr))<0)	
 	{
-		// err_sys("connect error");
-		printf("connect error");
+		err_sys("connect error");
+		// printf("connect error");
 		exit(1);
 	}
 
@@ -37,14 +37,14 @@ int main(int argc,char **argv){
 		recvline[n]=0;
 		if (fputs(recvline,stdout)==EOF)
 		{
-			// err_sys("fputs error");
-			printf("fputs error");
+			err_sys("fputs error");
+			// printf("fputs error");
 			exit(1);
 		}
 	}
 	if (n<0){
-		// err_sys("read error");
-		printf("read error");
+		err_sys("read error");
+		// printf("read error");
 		exit(1);
 	}
 
